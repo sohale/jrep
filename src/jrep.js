78e90a3164d38284f15b5e4d6edee94100422621
Sohail Siadatnejad <sohale@gmail.com>
2022-05-12 21:29:49 +0100

    forked from @sohale:snippets/javascript/

---
+++ src/jrep.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+/**
+* Usage: neat-jsfilter.js  <filter1>  <filter2> ...
+*
+* * Each <filter> is a map, (a string transformation), a javascript function with arg `x`..
+* * The <filter>s are applied sequentially on each line in the piped content.
+* * In case of exception in any of the <filter>s, that line is removed.
+*
+Example:
+```
+cat s1-log.log  |  node neat-jsfilter.js  '11+x' 'x+55'
+
+cat s1-log.log   |   node neat-jsfilter.js     '/"GET \/new-registry\/(.*)" "/.exec(x)[1]'
+
+cat s1-log.log | \
+    node neat-jsfilter.js \
+        '/"GET \/new-registry\/(.*)" "/.exec(x)[1]' | \
+            xargs -n 1 -I {}  \
+               bash -c "download0 {}"
+```
+*/
+
+const fs = require("fs"), util = require("util");
+const ERR_EXCEPTIONS = process.env('ERR_EXCEPTIONS'), DEBUG_ARGS = process.env('DEBUG_ARGS');
+
+const [_,__,...maps] = process.argv;
+const mapsf = maps.map( exprStr => (x => eval('x=>'+exprStr)(x) ) );
+/* ... ∘ mapsf[1] ∘ mapsf[0] ∘ (x) */
+const rec = (x, mapsf) => (mapsf.length===0)?x : rec(mapsf[0](x), mapsf.slice(1))
+
+const skipException = (x,f) => {try{return f(x);}catch(e){ERR_EXCEPTIONS && console.error(e); return null;}};
+const transfline = x => skipException(x, x=>rec(x,mapsf));
+
+const transffile = inpstr => inpstr.split('\n').map(transfline).filter(x=>x!==null).join('\n');
+const read_from_stdin = () => fs.readFileSync(0).toString();
+console.log(transffile(read_from_stdin()));
+
+DEBUG_ARGS && console.error('x=>'+maps);
+//  forked from https://github.com/sohale/snippets/blob/master/javascript/neat-filter-obfuscated.js
+
+/*
+forked from https://github.com/sohale/snippets/blob/master/javascript/neat-jsfilter.js
+*/
