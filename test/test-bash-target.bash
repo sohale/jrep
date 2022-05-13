93fcff33ff5d6dbd146ff4d7cd91ff7e19d2b4ac
Sohail Siadatnejad <sohale@gmail.com>
2022-05-13 16:46:52 +0100

    remove unnecessary error logs from test. btw, the test works

--- test/test-bash-target.bash
+++ test/test-bash-target.bash
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 
-export ERR_EXCEPTIONS=1
+#export ERR_EXCEPTIONS=1
 export DEBUG_ARGS=1
 
 find ./src | \
     node -e "$(curl -L https://raw.githubusercontent.com/sohale/jrep/main/src/jrep.js | tail -n +2)" \
        - \
        '/\/([^\/]*\.js)$/.exec(x)[1]'
+
+# output:  jrep.js
